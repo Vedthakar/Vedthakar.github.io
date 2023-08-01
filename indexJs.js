8ec8a1a8115fb212e0c197101846f91ce0eca050
Vedthakar <111605341+Vedthakar@users.noreply.github.com>
2023-07-31 22:09:47 -0600

    Add files via upload

---
+++ indexJs.js
@@ -0,0 +1,27 @@
+let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("mySlides");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
+var counter = 1;
+setInterval(function(){
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if(counter > 4){
+    counter = 1;
+  }
+}, 3000);
+
+
+document.body.style.zoom="67%"
